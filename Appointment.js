1cc33ecf8e790b6c532e9f08d889d08465afed43
nikita3006 <nikitanegi3011@gmail.com>
2023-10-24 09:35:31 +0530

    update code

--- Appointment.js
+++ Appointment.js
@@ -1,71 +1,89 @@
-function saveToLocalStorage(event) {
+function showNewUserOnScreen(user) {
+    const parentNode = document.getElementById('listOfUsers');
+    const childHTML = `<li id="${user._id}">
+      <span>Name: ${user.name}</span>
+      <span>Email: ${user.email}</span>
+      <span>Phone: ${user.mobile}</span>
+      <button onclick="deleteUser('${user._id}')">Delete User</button>
+      <button onclick="editUser('${user._id}', '${user.name}', '${user.email}', '${user.mobile}')">Edit User</button>
+    </li>`;
+  
+    parentNode.insertAdjacentHTML('beforeend', childHTML);
+  }
+  
+  function editUser(userId, name, email, mobile) {
+    document.getElementById('name').value = name;
+    document.getElementById('email').value = email;
+    document.getElementById('mobile').value = mobile;
+  
+    
+    const userIdInput = document.createElement('input');
+    userIdInput.type = 'hidden';
+    userIdInput.name = 'userId';
+    userIdInput.value = userId;
+    document.getElementById('my-form').appendChild(userIdInput);
+  }
+  
+  function deleteUser(userId) {
+    
+  }
+  
+  function saveToLocalStorage(event) {
     event.preventDefault();
     const name = event.target.name.value;
     const email = event.target.email.value;
     const mobile = event.target.mobile.value;
-    //localStorage.setItem('name', name);
-    //localStorage.setItem('email', email);
+    const userId = event.target.userId.value; 
+  
     const obj = {
-        name,
-        email,
-        mobile
-    }
-    axios.post("https://crudcrud.com/api/9a308f5019bf4607859368db73e5ae6a/appiontmentData", obj)
+      name,
+      email,
+      mobile,
+    };
+  
+    if (userId) {
+     
+      axios
+        .put(`https://crudcrud.com/api/9a308f5019bf4607859368db73e5ae6a/appiontmentData/${userId}`, obj)
         .then((response) => {
-            
-            showNewUserOnScreen(response.data); 
-            console.log(response);
+         
         })
         .catch((error) => {
-            document.body.innerHTML = document.body.innerHTML + '<h4>Something went wrong</h4>'
-            console.log(error);   
+         
+        });
+    } else {
+     
+      axios
+        .post('https://crudcrud.com/api/9a308f5019bf4607859368db73e5ae6a/appiontmentData', obj)
+        .then((response) => {
+          showNewUserOnScreen(response.data);
         })
-    showNewUserOnScreen(obj);
+        .catch((error) => {
+          
+        });
+    }
+  
     
-}
-window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/9a308f5019bf4607859368db73e5ae6a/appiontmentData")
-        .then((response) => {
-            console.log(response);
-            for (var i=0; i<response.data.length; i++) {
-                showNewUserOnScreen(response.data[i]);
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-})
-function showNewUserOnScreen (user) {
-    document.getElementById('email').value='';
-    document.getElementById('name').value='';
-    document.getElementById('mobile').value='';
-    const parentNode = document.getElementById('listOfUsers');
-    const childHTML = `<li id=${user._id}> ${user.name} - ${user.email} - ${user.mobile}
-    <button onclick=deleteUser('${user._id}')> Delete User </button> 
-    <button onclick=editUser('${user._id}','${user.name}','${user.mobile}')> Edit User </button>
-    <button onclick=editUser('${user.email}','${user.name}','${user.mobile}','${user._id}')> Edit User </button>
-    </li>`
-
-}
-function removeUserFromScreen(userId){
-    const parentNode = document.getElementById('listOfUsers');
-    const childNodeToBeDeleted = document.getElementById(userId);
-
-    parentNode.removeChild(childNodeToBeDeleted)
-    if(childNodeToBeDeleted) {
-        parentNode.removeChild(childNodeToBeDeleted)
+    document.getElementById('my-form').reset();
+    const userIdInput = document.querySelector('input[name="userId"]');
+    if (userIdInput) {
+      userIdInput.remove();
     }
+  }
+  
 
-}
+  document.getElementById('my-form').addEventListener('submit', saveToLocalStorage);
+  
 
-function editUser(emailId, name, mobile) {
-function editUser(emailId, name, mobile,userId) {
-    document.getElementById('email').value=emailId;
-    document.getElementById('name').value=name;
-    document.getElementById('mobile').value=mobile;
-
-    deleteUser(emailId)
-    deleteUser(userId)
-
-}
-}
+  window.addEventListener("DOMContentLoaded", () => {
+    axios.get("https://crudcrud.com/api/9a308f5019bf4607859368db73e5ae6a/appiontmentData")
+      .then((response) => {
+        for (var i = 0; i < response.data.length; i++) {
+          showNewUserOnScreen(response.data[i]);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  });
+  
